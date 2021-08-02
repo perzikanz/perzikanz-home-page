3a63ee40a7a02d3fe567a4a93d83628a18c32fef
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-08-02 15:55:08 +0900

    widthとheightを削除

--- src/components/ImageWrapper.tsx
+++ src/components/ImageWrapper.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 
 const ImageWrapper = ({
   src,
-  width,
   alt,
-  height,
   className,
 }: {
   src: string;
   alt: string;
-  width: number;
-  height: number;
   className: string;
 }) => {
   return (
     <>
       <img
         src={`${process.env.NEXT_PUBLIC_STORAGE_DOMAIN}/${src}`}
-        width={width}
         alt={alt}
-        height={height}
         className={className}
       />
     </>
