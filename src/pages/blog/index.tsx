110a541f05dfbc02e9051bd78ac89e2748275778
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-08-04 16:04:29 +0900

    ヘッダーとフッターを追加

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 import BlogTitles from '../../components/blog/BlogTitles';
 
-import style from '../../styles/BlogTop.module.css';
+import styles from '../../styles/BlogTop.module.css';
 
 const BlogTop = () => {
   return (
@@ -13,10 +15,15 @@ const BlogTop = () => {
         <meta name='description' content='' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <h1 className={style.page_title}>記事一覧</h1>
+
+      <Header />
+
+      <main className={styles.main_container}>
+        <h1 className={styles.page_title}>記事一覧</h1>
         <BlogTitles titleNumber={100} />
       </main>
+
+      <Footer />
     </>
   );
 };
