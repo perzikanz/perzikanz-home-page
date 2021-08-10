8c179ce9329ad4060dc065d1b67d23d3ea7a7249
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-08-10 17:42:16 +0900

    URLの問題を解決

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   assetPrefix: isProd ? process.env.STORAGE_DOMAIN : '',
   basePath: process.env.SUB_DIRECTORY,
+  trailingSlash: true,
   reactStrictMode: true,
 };
